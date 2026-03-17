418fc212bc0b9327bc585ac64efc6da1e91cd4b0
gNOR-mu <gabriel.norambuenam@gmail.com>
2026-03-17 02:50:20 -0300

    Configuración inicial de seguridad añadida

---
+++ src/main/java/com/bootcamp/mvp_m6/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.bootcamp.mvp_m6.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        //TODO
+
+        http.authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/", "/login").permitAll()
+                        //TODO CAMBIAR
+                        .anyRequest().permitAll()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/")
+                );
+
+        return http.build();
+    }
+
+
+}
