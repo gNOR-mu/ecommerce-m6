27dbb660471318724ba0f99d0d10f5cc3e198576
gNOR-mu <gabriel.norambuenam@gmail.com>
2026-03-17 02:54:21 -0300

    GlobalControllerAdvice añadido

---
+++ src/main/java/com/bootcamp/mvp_m6/infrastructure/adapters/input/web/GlobalControllerAdvice.java
@@ -0,0 +1,22 @@
+package com.bootcamp.mvp_m6.infrastructure.adapters.input.web;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@ControllerAdvice(basePackages = "com.bootcamp.mvp_m6.infrastructure.adapters.input.web")
+public class GlobalControllerAdvice {
+
+    /*Inyecta el nombre de la aplicación desde el properties*/
+    @Value("${app.app-name}")
+    private String appName;
+
+    /**
+     * Obtiene el nombre "comercial" de la aplicación
+     * @return nombre comercial de la app
+     */
+    @ModelAttribute("appName")
+    public String populateAppName(){
+        return appName;
+    }
+}
