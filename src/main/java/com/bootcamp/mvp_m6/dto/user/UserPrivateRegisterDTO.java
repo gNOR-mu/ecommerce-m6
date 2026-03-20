c7f0b138e19ed86e58066600aff9ebb8a004fe8f
gNOR-mu <gabriel.norambuenam@gmail.com>
2026-03-20 18:30:09 -0300

    DTOs añadidos

---
+++ src/main/java/com/bootcamp/mvp_m6/dto/user/UserPrivateRegisterDTO.java
@@ -0,0 +1,38 @@
+package com.bootcamp.mvp_m6.dto.user;
+
+import com.bootcamp.mvp_m6.enums.Role;
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class UserPrivateRegisterDTO {
+
+    @NotBlank(message = "El nombre no puede estar en blanco")
+    private String name;
+
+    @NotBlank(message = "El rut no puede estar en blanco")
+    private String rut;
+
+    private String cellphone;
+
+    @NotNull
+    @Email(message = "Correo inválido")
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank(message = "La contraseña no puede estar en blanco")
+    @Size(min = 8, message = "El largo mínimo para la contraseña son 8 caracteres")
+    @Size(max = 100, message = "El largo máximo para la contraseña son 100 caracteres")
+    private String password;
+
+    @NotNull
+    private Role role;
+}
---
+++ src/main/java/com/bootcamp/mvp_m6/dto/user/UserPublicRegisterDTO.java
@@ -0,0 +1,31 @@
+package com.bootcamp.mvp_m6.dto.user;
+
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class UserPublicRegisterDTO {
+
+    @NotBlank(message = "El nombre no puede estar en blanco")
+    private String name;
+
+    @NotBlank(message = "El rut no puede estar en blanco")
+    private String rut;
+
+    private String cellphone;
+
+    @NotNull
+    @Email(message = "Correo inválido")
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank(message = "La contraseña no puede estar en blanco")
+    @Size(min = 8, message = "El largo mínimo para la contraseña son 8 caracteres")
+    @Size(max = 100, message = "El largo máximo para la contraseña son 100 caracteres")
+    private String password;
+}
