78a2ba46d653c1f507a2eba9967cbf2f2be23662
gNOR-mu <gabriel.norambuenam@gmail.com>
2026-03-20 17:34:32 -0300

    web security activada y passwordEncoder añadido

--- src/main/java/com/bootcamp/mvp_m6/config/SecurityConfig.java
+++ src/main/java/com/bootcamp/mvp_m6/config/SecurityConfig.java
@@ -1,18 +1,25 @@
 package com.bootcamp.mvp_m6.config;
 
+import com.bootcamp.mvp_m6.enums.Role;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         //TODO
 
         http.authorizeHttpRequests(auth -> auth
                         .requestMatchers("/", "/login").permitAll()
+                        .requestMatchers("/admin/**").hasRole(Role.ADMIN.name())
                         //TODO CAMBIAR
                         .anyRequest().permitAll()
                 )
@@ -27,5 +34,8 @@ public class SecurityConfig {
         return http.build();
     }
 
-
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
