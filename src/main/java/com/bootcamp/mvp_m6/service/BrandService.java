95bd2eafb4b8f24c946477eea52f4c79c9ae59d7
gNOR-mu <gabriel.norambuenam@gmail.com>
2026-03-21 15:33:30 -0300

    Creación de marca implementada

--- src/main/java/com/bootcamp/mvp_m6/service/BrandService.java
+++ src/main/java/com/bootcamp/mvp_m6/service/BrandService.java
@@ -1,5 +1,7 @@
 package com.bootcamp.mvp_m6.service;
 
+import com.bootcamp.mvp_m6.exceptions.InvalidOperationException;
+import com.bootcamp.mvp_m6.model.Brand;
 import com.bootcamp.mvp_m6.repository.BrandRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,4 +12,12 @@ public class BrandService {
 
     private final BrandRepository brandRepository;
 
+
+    public Brand create(Brand brand) {
+        if (brandRepository.existsByName(brand.getName())) {
+            throw new InvalidOperationException("Ya existe un producto con el nombre: " + brand.getName());
+        }
+
+        return brandRepository.save(brand);
+    }
 }
