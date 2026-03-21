2f686e2ea4039b3c144dd8533e18d1788b79eb92
gNOR-mu <gabriel.norambuenam@gmail.com>
2026-03-21 15:34:12 -0300

    Creación de categoría añadida

--- src/main/java/com/bootcamp/mvp_m6/service/CategoryService.java
+++ src/main/java/com/bootcamp/mvp_m6/service/CategoryService.java
@@ -1,5 +1,7 @@
 package com.bootcamp.mvp_m6.service;
 
+import com.bootcamp.mvp_m6.exceptions.InvalidOperationException;
+import com.bootcamp.mvp_m6.model.Category;
 import com.bootcamp.mvp_m6.repository.CategoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,4 +12,11 @@ public class CategoryService {
 
     private final CategoryRepository categoryRepository;
 
+
+    public Category create(Category category){
+        if(categoryRepository.existsByName(category.getName())){
+            throw new InvalidOperationException("Ya existe una categoría con el nombre: " + category.getName());
+        }
+        return categoryRepository.save(category);
+    }
 }
