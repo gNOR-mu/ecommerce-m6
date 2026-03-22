b0e540e639653fd05d9c80a92aeb0d57105f4a02
gNOR-mu <gabriel.norambuenam@gmail.com>
2026-03-22 16:54:46 -0300

    página de información de producto añadida

--- src/main/java/com/bootcamp/mvp_m6/controller/ProductController.java
+++ src/main/java/com/bootcamp/mvp_m6/controller/ProductController.java
@@ -1,10 +1,12 @@
 package com.bootcamp.mvp_m6.controller;
 
 import com.bootcamp.mvp_m6.service.ProductService;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -18,5 +20,12 @@ public class ProductController {
     public String products(Model model){
         model.addAttribute("products", productService.findAllResume());
         return "products";
+    }
+
+    @GetMapping("/{id}")
+    public String products(Model model,
+                           @NotNull @PathVariable Long id){
+        model.addAttribute("product", productService.findInfoById(id));
+        return "productId";
     }
 }
